682134499c32bb8c704e3f96dfc84c2acd277291
mrogovets <34492317+mrogovets@users.noreply.github.com>
2020-06-19 21:56:20 +0300

    finishd 4-th day. Slider + tabs: actors/creators

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 $(document).ready(function () {
-    const mMenuBtn = $(".m-menu-button"),
-        mMenu = $(".m-menu");
-    mMenuBtn.on('click', function () {
-        mMenu.toggleClass('active');
-        $("budy").toggleClass("no-scroll")
-    });
+  const mMenuBtn = $(".m-menu-button"),
+    mMenu = $(".m-menu"),
+    tab = $(".tab");
+  mMenuBtn.on("click", function () {
+    mMenu.toggleClass("active");
+    $("budy").toggleClass("no-scroll");
+  });
+
+  tab.on("click", function () {
+    tab.removeClass("active");
+    $(this).toggleClass("active");
+    let activeTabContent = $(this).attr("data-target");
+    $(".tabs-content").removeClass("visible");
+    $(activeTabContent).toggleClass("visible");
+  });
+
+  var mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        slidesOffsetAfter: 50,
+        navigation: {
+          nextEl: ".button-next",
+        },
+      },
+    },
+  });
 });
-// mMenu 4-d day
